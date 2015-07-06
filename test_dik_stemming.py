a139003a88d955e389ad9451e52931553fc1c464
James Tauber <jtauber@jtauber.com>
2015-07-05 21:31:43 -0400

    added lexicon and stemming tests for Dik's nifty handouts

---
+++ test_dik_stemming.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import yaml
+
+from characters import strip_length
+
+from stemming import Stemmer
+
+
+TEST_FILES = [
+    "tests/dik.yaml"
+]
+
+
+stemmer = Stemmer("lexicons/dik.yaml")
+
+for TEST_FILE in TEST_FILES:
+    with open(TEST_FILE) as f:
+        for test in yaml.load(f):
+            lemma = strip_length(test.pop("lemma"))
+            test_length = test.pop("test_length", True)
+            location = test.pop("location", "")
+
+            for parse, forms in test.items():
+                for form in forms.split("/"):
+                    norm = strip_length(form)
+
+                    stemmer.stem(location, lemma, parse, norm)
+
+stemmer.counter.results()
