a2d9379854f1ed13bec1276c0dd2eaa0f12f14ac
James Tauber <jtauber@jtauber.com>
2015-08-01 15:51:07 -0400

    factored out common test_stemming code from pratt/dik cases

--- test_pratt_stemming.py
+++ test_pratt_stemming.py
@@ -1,27 +1,5 @@
 #!/usr/bin/env python3
 
-import yaml
+from test_stemming import test
 
-from characters import strip_length
-
-from stemming import Stemmer
-
-
-TEST_FILES = [
-    "tests/pratt.yaml"
-]
-
-
-stemmer = Stemmer("lexicons/pratt.yaml")
-
-for TEST_FILE in TEST_FILES:
-    with open(TEST_FILE) as f:
-        for test in yaml.load(f):
-            lemma = strip_length(test.pop("lemma"))
-            test_length = test.pop("test_length", True)
-            location = test.pop("location", "")
-
-            for parse, form in test.items():
-                stemmer.stem(location, lemma, parse, form, test_length)
-
-stemmer.counter.results()
+test("tests/pratt.yaml", "lexicons/pratt.yaml")
---
+++ test_stemming.py
@@ -0,0 +1,20 @@
+import yaml
+
+from characters import strip_length
+
+from stemming import Stemmer
+
+
+def test(test_file, lexicon_file):
+    stemmer = Stemmer(lexicon_file)
+
+    with open(test_file) as f:
+        for test in yaml.load(f):
+            lemma = strip_length(test.pop("lemma"))
+            test_length = test.pop("test_length", True)
+            location = test.pop("location", "")
+
+            for parse, form in test.items():
+                stemmer.stem(location, lemma, parse, form, test_length)
+
+    stemmer.counter.results()
